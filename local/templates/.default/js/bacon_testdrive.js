e87496212d256781e884910e7603069aae9ddcf0
Natalya Chigryay <nchigryay@articul.office>
2017-04-07 19:37:01 +0300

    bacon js for mtutorial started

--- local/templates/.default/js/bacon_testdrive.js
+++ local/templates/.default/js/bacon_testdrive.js
@@ -1,2 +1,28 @@
-import 'baconjs';
+import Bacon from 'baconjs';
+import 'baconui';
 
+
+let form = $('#register-form');
+let regBtn = form.find('button[type="submit"]');
+let usernameField = form.find("#username input");
+let fullnameField = form.find("#fullname input");
+
+// function textFieldValue(textField) {
+//     function value() { return textField.val() }
+//     return textField.asEventStream("keyup").map(value).toProperty(value())
+// }
+
+let username = Bacon.UI.textFieldValue(usernameField);
+let fullname = Bacon.UI.textFieldValue(fullnameField);
+
+function and(a,b) { return a && b }
+function nonEmpty(x) { return x.length > 0 }
+function setEnabled(el, x) { return el.attr("disabled", x); }
+
+let usernameEntered = username.map(nonEmpty)
+let fullnameEntered = fullname.map(nonEmpty)
+
+let buttonEnabled = usernameEntered.combine(fullnameEntered, and);
+
+// buttonEnabled.not().onValue(regBtn, "attr", "disabled");
+buttonEnabled.not().assign(setEnabled, regBtn);
